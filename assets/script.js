edca506a84a7d4775600990b7c70d2f557241109
Gabby Hernandez <gabbyhdezz697@gmail.com>
2022-02-24 18:12:54 -0500

    update to coding quiz

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var startButton = document.querySelector(".start-button");
+
+startButton.addEventListener('click', function() {
+   init()
+})
+
+//3. Initializing: once clicked the header and sttart button hide and displays question and options and timer begins
+
+function init() {
+    console.log(startButton)
+}
+
